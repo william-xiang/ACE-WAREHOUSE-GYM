c82dc3944a02f51f9512194a91ab9a526269909f
William <william.xiang1375@gmail.com>
2018-06-11 21:48:36 -0400

    Initial commit

---
+++ joinUs.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>ACE | JoinUs</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/calendar.css" />
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro|Quicksand|Raleway|Rozha+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/calendar.js"></script>
+    <link rel="shortcut icon" href="img/smalllogo.png" />
+</head>
+
+<body class="joinus">
+    <a class="anchor slideshow" id="anchor"></a>
+
+    <div class="header" id="navigation">
+        <ul>
+            <li>
+                <a href="index.html"><img class="logo" src="img/logo.png"></a>
+            </li>
+            <li>
+                <a href="classes.html">Classes</a>
+            </li>
+            <li>
+                <a href="trainers.html">Trainers</a>
+            </li>
+            <li>
+                <a  href="clients.html">Clients</a>
+            </li>
+            <li>
+                <a  href="programs.html">Programs</a>
+            </li>
+            <li>
+            <a  href="business.html">Business</a>
+            </li>
+            <li>
+                <a href="joinUs.php">Join Us</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="containterContact">
+        <ul class="ulcontainer">
+            <li class="licontainer">
+                <div class="calendar">
+                    <div id="header">
+                        <div id="date">
+                            <p id="datetext">
+    
+                            </p>
+                        </div>
+                        <div id="today">
+                            <button id="todaybutton">Today</button>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="datelist">
+                            <div id="datelist">
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week1">
+    
+                                    </div>
+                                    <div class="day" id="day1">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle1">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator1">
+    
+                                    </div>
+                                </div>
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week2">
+    
+                                    </div>
+                                    <div class="day" id="day2">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle2">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator2">
+    
+                                    </div>
+                                </div>
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week3">
+    
+                                    </div>
+                                    <div class="day" id="day3">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle3">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator3">
+    
+                                    </div>
+                                </div>
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week4">
+    
+                                    </div>
+                                    <div class="day" id="day4">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle4">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator4">
+    
+                                    </div>
+                                </div>
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week5">
+    
+                                    </div>
+                                    <div class="day" id="day5">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle5">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator5">
+    
+                                    </div>
+                                </div>
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week6">
+    
+                                    </div>
+                                    <div class="day" id="day6">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle6">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator6">
+    
+                                    </div>
+                                </div>
+                                <div class="date notcurrentdate">
+                                    <div class="week" id="week7">
+    
+                                    </div>
+                                    <div class="day" id="day7">
+    
+                                    </div>
+                                    <div class="calendarcircle" id="circle7">
+    
+                                    </div>
+                                    <div class="indicator" id="indicator7">
+    
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="w3-button w3-display-left w3-black">&#10094;</button>
+                            <button class="w3-button w3-display-right w3-black">&#10095;</button>
+                        </div>
+                        <div class="agenda">
+    
+                        </div>
+    
+                        <div class="detail" id="myModal">
+                            <!-- Modal content -->
+                            <div class="modal-content">
+                                <div class="modal-body">
+    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+            </li>
+            <li class="licontainer">
+                    <div class="contact">
+                    <p id="title">We welcome your<br> concern!</p>
+                    <p id="address">
+                        <span>931 Progress Ave unit 13</span>
+                        <br>
+                        <span>Scarborough, ON M1G 3V5</span>
+                    </p>
+                    <p id="phone">
+                        <span>Tel: 647&#8208;606&#8208;9655</span>
+                    </p>
+                    <p id="wechat">Wechat: teamace2016</p>
+                    <p id="instagram">Instagram: acewarehousegym</p>
+    
+                    <form name="contactus" onsubmit="return validateForm()" action="" method="post">
+                        <input type="text" placeholder="Name *" name="name" required>
+                        <br>
+                        <input type="text" placeholder="Email *" name="email" required>
+                        <br>
+                        <input type="text" placeholder="Phone" name="phone">
+                        <br>
+                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                        <br>
+                        <input id="button" type="submit" value="Send">
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="footer">
+        <div class="author">
+            <p>&copy; 2018 by Cherrina Hong.</p>
+        </div>
+        <div class="backtop">
+            <a href="#anchor"><i class="fas fa-angle-up"></i></a>
+        </div>
+    </div>    
+</body>
+
+</html>
+
+<?php
+if(isset($_POST['email'])) {
+
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $phone = $_POST['phone']; // not required
+    $message = $_POST['message']; // not required
+
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to1 = "info@acewarehousegym.ca";
+    $email_subject1 = "Form details";
+    $email_to2 = $email;
+    $email_subject2 = "Thanks for contacting us!";
+
+    
+    $email_message1 = "Form details below.\n\n";
+    $email_message1 .= "Name: ".$name."\n";
+    $email_message1 .= "Email: ".$email."\n";
+    $email_message1 .= "Telephone: ".$phone."\n";
+    $email_message1 .= "Message: ".$message."\n";
+
+    $email_message2 = "Thank you for contacting us. We will be in touch with you very soon.\n\n";
+
+    
+    // create email headers
+    $headers1 = "From: ACE Warehouse Gym<$email>"."\r\n" ."Reply-To: ACE Warehouse Gym<$email>"."\r\n" .'X-Mailer: PHP/' . phpversion();
+    $headers2 = "From: ACE Warehouse Gym<$email_to1>"."\r\n" ."Reply-To: ACE Warehouse Gym<$email_to1>"."\r\n" .'X-Mailer: PHP/' . phpversion();
+
+    $status = mail($email_to1, $email_subject1, $email_message1, $headers1);  
+    mail($email_to2, $email_subject2, $email_message2, $headers2); 
+}
+?>
---
+++ js/main.js
@@ -0,0 +1,186 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      } 
+    });
+
+    $(".businesstop").click(function() {
+      $(".businesstop").removeClass(" activebusinesstop");
+      $(this).addClass(" activebusinesstop");
+    });
+
+    $(".businesstop").mouseover(function() {
+      $(".businesstop").removeClass(" activebusinesstop");
+      $(this).addClass(" activebusinesstop");
+    });
+
+    $(".businesstop").mouseleave(function() {
+      $(".businesstop").removeClass(" activebusinesstop");
+    });
+
+    $(".businesstoggle").mouseover(function() {
+      $(".businesstop").removeClass(" activebusinesstop");
+      $(this).siblings().addClass(" activebusinesstop");
+    });
+
+    $(".businesstoggle").mouseleave(function() {
+      $(".businesstop").removeClass(" activebusinesstop");
+    });
+  });
+
+document.addEventListener('scroll', function() {
+  var circle = document.getElementsByClassName("circle");
+  var businesscircle = document.getElementsByClassName("businesscircle");
+
+  if (circle.length != 0) {
+    for (i = 0; i < circle.length; i++) {
+      circle[i].className = circle[i].className.replace(" activeslide", "");
+    }
+  }
+
+  if (businesscircle.length != 0) {
+    for (i = 0; i < businesscircle.length; i++) {
+      businesscircle[i].className = businesscircle[i].className.replace(" businessactiveslide", "");
+    }
+  }
+
+
+  var y_scroll_pos = window.pageYOffset;
+
+  if(y_scroll_pos < 630) {
+    index = 1;
+  }
+  else if (y_scroll_pos >= 630 && y_scroll_pos < 1680) {
+    index = 2;
+  }
+  else {
+    index = 3;
+  }
+
+  if(y_scroll_pos < 130) {
+    businessindex = 1;
+  }
+  else {
+    businessindex = 2;
+  }
+
+  if (businesscircle.length != 0) {
+    businesscircle[businessindex - 1].className += " businessactiveslide";
+  }
+
+  if (circle.length != 0) {
+    circle[index - 1].className += " activeslide";
+  }
+}, false);
+
+var slideIndex = 1;
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  var circle = document.getElementsByClassName("circle");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < circle.length; i++) {
+    circle[i].className = circle[i].className.replace(" activeslide", "");
+  }
+  circle[slideIndex-1].className += " activeslide";
+}
+
+function contact() {
+  alert("Thank you for contacting with us!");
+}
+
+function validateForm() {
+  var x = document.forms["contactus"]["name"].value;
+  var y = document.forms["contactus"]["email"].value;
+
+  if (x == "") {
+      alert("Name must be filled out");
+      return false;
+  }
+  if (y == "") {
+    alert("Email must be filled out");
+    return false;
+}
+
+  alert("Thank you for contacting with us!");
+}
+
+function overPart1() {
+  var content = document.getElementsByClassName("content");
+  content[0].className += " contentActive";
+}
+
+function outPart1() {
+  var content = document.getElementsByClassName("content");
+  content[0].className = content[0].className.replace(" contentActive", "");
+}
+
+
+//slider in business
+function slide() {
+  carousel();
+}
+
+var slideIndex = 0;
+var x = document.getElementsByClassName("slides");
+var timer;
+
+function carousel() {
+    var i;
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}    
+    x[slideIndex-1].style.display = "block";  
+    timer = setTimeout(carousel, 4000); // Change image every 2 seconds
+}
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+function pause() {
+  clearTimeout(timer);
+}
+
+function resume() {
+  timer = setTimeout(carousel, 4000);
+}
+
+window.onclick = function(event) {
+  if (event.target.className != "businesstoptext") {
+    $(".businesstop").removeClass(" activebusinesstop");
+  }
+}
+
+
+
+
